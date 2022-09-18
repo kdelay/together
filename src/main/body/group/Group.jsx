3b29d1395f509291fdf102353a7440d1fafa0667
김지연 <kdelay20@gmail.com>
2022-09-18 19:30:34 +0900

    [main - Body] Grid 표시
    - grid속성으로 main Body 요소들을 추가했다.

--- src/main/body/group/Group.jsx
+++ src/main/body/group/Group.jsx
@@ -8,7 +8,6 @@ function Group() {
     <div className="main-body">
       <h4>꿈과 희망이 넘치는 1003호</h4>
       <div className='card-body'>
-        <div className='card-group'>
         <Card id="card-group1">
           <Card.Header as="h6">그룹원</Card.Header>
           <Card.Body>
@@ -20,9 +19,7 @@ function Group() {
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>
-        </div>
 
-        <div className='card-group'>
         <Card id="card-group2">
           <Card.Header as="h6">캘린더</Card.Header>
           <Card.Body>
@@ -34,7 +31,56 @@ function Group() {
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>
-        </div>
+
+        <Card id="card-group3">
+          <Card.Header as="h6">미션</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+
+      <div className="card-body2">
+      <Card id="card-group4">
+          <Card.Header as="h6">오늘의 미션</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card id="card-group2">
+          <Card.Header as="h6">달성 뱃지</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card id="card-group3">
+          <Card.Header as="h6">갤러리</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
